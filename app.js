4175cc9293abed3af9621760cc1d86d2f163577e
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-28 09:27:51 +0430

    init

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 import presentDrinks from './src/presentDrinks.js'
 
 const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a"
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+})
