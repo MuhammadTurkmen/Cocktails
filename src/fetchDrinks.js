0fa02bc085b3a03646e37770e351344b2b27484a
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 11:11:05 +0430

    init

--- src/fetchDrinks.js
+++ src/fetchDrinks.js
@@ -1,6 +1,7 @@
 import { showLoading } from "./toggleLoading.js"
 
 const fetchDrinks = async (url) => {
+    showLoading()
     try {
         const response = await fetch(url) 
         const data = await response.json()
