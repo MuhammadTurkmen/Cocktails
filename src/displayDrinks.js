303800a0496e98ab5963956611ba6bf9e24f34bb
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 11:12:09 +0430

    init

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -7,6 +7,7 @@ const displayDrinks = ({drinks}) => {
     const title = get('.title')
     if(!drinks) {
         // hide loading
+        hideLoading()
         title.textContent = `sorry, no drinks matched your search`
         section.innerHTML = null
         return
