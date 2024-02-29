f16bc70f0e17742ba2b26e5f3be64ccc124e4be8
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 10:42:24 +0430

    init

--- src/searchForm.js
+++ src/searchForm.js
@@ -2,3 +2,5 @@ import get from './getElement.js'
 import presentDrinks from './presentDrinks.js'
 
 const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s'
+
+const form = get('.search-form')
