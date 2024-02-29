855d0399f5c401788ba81ccb3bad11f4a44bcd32
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 09:55:51 +0430

    init

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -3,6 +3,7 @@ import get from './getElement.js'
 const displayDrinks = ({drinks}) => {
     const section = get('.section-center')
     const title = get('.title')
+    console.log(drinks);
 }
 
 export default displayDrinks
