3cb1118b60308009d7774b03d537c0e6fc756ef3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 10:31:17 +0430

    init

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -13,7 +13,7 @@ const displayDrinks = ({drinks}) => {
         const {idDrink: id, strDrink:name, strDrinkThumb: image} = drink 
         return `<a href="drink.html">
                     <article class="cocktail" data-id="${id}">
-                    <img src="${image}" alt="cocktail">
+                    <img src="${image}" alt="${name}">
                     <h3>martini</h3>
                     </article>
                 </a>`
