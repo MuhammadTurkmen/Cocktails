54625d097e29f5d7022dd3fd039764f645ccdbff
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 09:43:13 +0430

    init

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,10 +1,12 @@
 import fetchDrinks from "./fetchDrinks.js";
 
 const showDrinks = async (url) => {
-    const data = await fetchDrinks(url)
-    console.log(url);
     // fetch drinks
+    const data = await fetchDrinks(url)
+    console.log(data);
+
     // display drinks
+    
 }
 
 export default showDrinks
