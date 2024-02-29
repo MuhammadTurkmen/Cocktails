468d805e63ccb59b38c0f8f562fc307f63eca6c4
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 10:02:33 +0430

    init

--- src/displayDrinks.js
+++ src/displayDrinks.js
@@ -10,7 +10,12 @@ const displayDrinks = ({drinks}) => {
         return
     }
     const newDrinks = drinks.map((drink) => {
-
+        return `a href="drink.html">
+                    <article class="cocktail" data-id="1">
+                    <img src="./cocktail.jpg" alt="cocktail">
+                    <h3>martini</h3>
+                    </article>
+                </a>`
     })
 }
 
