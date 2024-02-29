771cb89fb9fe919c637259d4f9b6b67850e95bca
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-29 09:39:20 +0430

    init

--- src/fetchDrinks.js
+++ src/fetchDrinks.js
@@ -4,6 +4,7 @@ const fetchDrinks = async (url) => {
     try {
         const response = await fetch(url) 
         const data = await response.json()
+        return data
     } catch (error) {
         
     }
