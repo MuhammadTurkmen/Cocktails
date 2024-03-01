b03058e4e8c7de0f4ea78b3c2c2c46e0aaa12d03
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-01 12:44:45 +0430

    init

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -2,10 +2,10 @@ import { hideLoading } from "./toggleLoading.js"
 import get from "./getElement.js"
 const displayDrink = (data) => {
     hideLoading()
-
+ 
     const drink = data.drinks[0]
     const {strDrinkThumb: image, strDrink: name, strInstructions: desc} = drink
  
-}  
+}   
  
 export default displayDrink 
