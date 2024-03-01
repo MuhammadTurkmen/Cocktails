06f66ee2a08b2231c5017b00b0760e9668461f9b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-01 12:31:24 +0430

    init

--- singleDrink.js
+++ singleDrink.js
@@ -3,6 +3,9 @@ import displayDrink from './src/displaySingleDrink.js'
 
 const presentDrink = async () => {
     const id = localStorage.getItem('drink')
+    if(!id) {
+        
+    }
     const drink = await fetchDrinks(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
     displayDrink(drink)
 }
