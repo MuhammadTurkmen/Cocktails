dd308041b117d47957bbe2fddd41ad67c77bb6a3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-01 12:25:20 +0430

    init

--- singleDrink.js
+++ singleDrink.js
@@ -2,7 +2,7 @@ import fetchDrinks from './src/fetchDrinks.js'
 import displayDrink from './src/displaySingleDrink.js'
 
 const presentDrink = async () => {
-
+    fetchDrinks()
 }
 
 window.addEventListener('DOMContentLoaded', presentDrink)
