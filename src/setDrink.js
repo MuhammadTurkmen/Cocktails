ca9a992b7a028646e110b4094ef490d4bd25749b
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-01 12:00:56 +0430

    init

--- src/setDrink.js
+++ src/setDrink.js
@@ -1,7 +1,7 @@
 const setDrink = (section) => {
     section.addEventListener('click', function(e) {
         e.preventDefault()
-        const id = e.target.parentElement
+        const id = e.target.parentElement.dataset.id
     })
 }
 
