8db2a7d9f510ae19f4411b249fbda8e3fe06586c
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-02 13:07:18 +0430

    init

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -15,7 +15,7 @@ const displayDrink = (data) => {
     const img = get('.drink-img')
     const drinkName = get('.drink-name')
     const description = get('.drink-desc')
-    const ingredients = get('.drink-ingredients')
+    const ingredients = get('.drink-ingredients') 
     img.src = image
     drinkName.textContent = name
     description.textContent = desc
