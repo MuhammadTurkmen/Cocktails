edcafee9f6d5c86c56b87020b7345aa713924086
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-02 12:51:34 +0430

    init

--- src/displaySingleDrink.js
+++ src/displaySingleDrink.js
@@ -6,7 +6,11 @@ const displayDrink = (data) => {
     const drink = data.drinks[0]
     const {strDrinkThumb: image, strDrink: name, strInstructions: desc} = drink
     const list = [
-        drink.strIngredient
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
     ]
 }   
   
